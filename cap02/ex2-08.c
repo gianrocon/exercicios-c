6fb033614cb02a8d7b166489ad85e3cc31c0811b
gianrocon <gianrocon@hotmail.com>
2021-03-02 20:48:06 -0300

    bitwise

---
+++ cap02/ex2-08.c
@@ -0,0 +1,1 @@
+/* Exercise 2-8. Write a function rightrot(x,n) that returns the value of the integer x rotated to the right by n bit positions. */
---
+++ cap02/getbits.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+unsigned getbits(unsigned x,int p,int n);
+
+int main(void)
+{
+    printf("%u", getbits((unsigned)8,3,2));
+}
+
+unsigned getbits(unsigned x,int p,int n)
+{
+    return (x >> (p+1-n)) & ~(~0 << n);
+}
