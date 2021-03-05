3f8c54bed345f1ab7f93efb1e843d2fd5da00804
gianrocon <gianrocon@hotmail.com>
2021-03-05 00:11:49 -0300

    retoques

--- cap02/getbits.c
+++ cap02/getbits.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 unsigned getbits(unsigned x,int p,int n);
 
 int main(void)
 {
-    printf("%u", getbits((unsigned)8,3,2));
+    printf("%u", getbits((unsigned)12,3,2)); 
+    //8,3,2 0000001000 => 000000000010 => 2
+    //12,3,2 0000001100 => 000000000011 => 3
 }
+
+
 
 unsigned getbits(unsigned x,int p,int n)
 {
