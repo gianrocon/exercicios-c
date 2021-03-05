f8622a6362c956dd24ed8041ea6b0e261935c6b6
gianrocon <gianrocon@hotmail.com>
2021-03-05 15:48:46 -0300

    ex2-08

--- cap02/ex2-07.c
+++ cap02/ex2-07.c
@@ -1,1 +1,24 @@
 /* Exercise 2-7. Write a function invert(x,p,n) that returns x with the n bits that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.  */
+#include <stdio.h>
+
+unsigned invert(unsigned x, int p, int n);
+
+int main()
+{
+    unsigned resultado;
+    unsigned x = 24; // 000000011000; => p=5 n=3  0000000100000 =>32
+    int p = 5;       // 000000111000 = 56
+    int n = 3;       // XOR---------
+                     // 000000100000
+
+    resultado = invert(x,p,n);
+    printf("%u\n", resultado);
+}
+
+unsigned invert(unsigned x, int p, int n)
+{
+    unsigned mask;
+    unsigned resultado;
+    mask = (~(~0 << n) << (p - n + 1));
+    return x ^ mask;
+} 
--- cap02/ex2-08.c
+++ cap02/ex2-08.c
@@ -1,1 +1,5 @@
 /* Exercise 2-8. Write a function rightrot(x,n) that returns the value of the integer x rotated to the right by n bit positions. */
+
+// x = 00000011110100100
+//                  1234 => n=4
+// x = 0000001111010
